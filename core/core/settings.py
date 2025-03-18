7a1017c68f715115e655987cc7b39b9af9a46274
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2025-03-18 15:55:40 +0600

    added vercel settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_fpe3Y2IjqXDB',
+        'HOST': 'ep-polished-mode-a5f0eq4a-pooler.us-east-2.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
